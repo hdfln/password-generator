4a4cc1aaefd7e9537dbcc5d23de8e64cc0ae929c
hide <hide@hongo.wide.ad.jp>
2023-01-11 00:09:47 +0900

    web plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'my_home_page.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
